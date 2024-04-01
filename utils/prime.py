537d9aeca364b62c7cafc22786a99e089465b76f
PanGan21 <ganelis.pan@gmail.com>
2024-04-02 01:31:43 +0300

    add prime utils

--- utils/prime.py
+++ utils/prime.py
@@ -1,7 +1,31 @@
 import random
 
-#  Uses https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+# generate with generator.py
+PRIMES_FILE = "primes.txt"
+
+
+def load_small_primes():
+    """
+    Load small prime numbers from the PRIMES_FILE.
+
+    Returns:
+        list: A list of small prime numbers.
+    """
+    return list(map(int, map(str.strip, open(PRIMES_FILE, 'r').readlines())))
+
+
 def is_prime(n, k=40):
+    """
+        Uses the Miller-Rabin primality test to check if a number is prime.
+        https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+
+        Args:
+            n (int): The number to check for primality.
+            k (int): The number of iterations for the Miller-Rabin test.
+
+        Returns:
+            bool: True if the number is prime, False otherwise.
+    """
     if n == 2:
         return True
 
